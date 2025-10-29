6d6f516230fa0f11477634f2f9e15f92efb6ac76
SharifulRiseup <sariful.i.riseuplabs@gmail.com>
2025-10-29 14:51:29 +0600

    added safe area

---
+++ Scripts/SafeArea/SafeArea.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace RiseupLabsSDK
+{
+    [RequireComponent(typeof(RectTransform))]
+    [AddComponentMenu("SKit/SafeArea")]
+    public class SafeArea : MonoBehaviour
+    {
+        private RectTransform panel;
+
+        private void Awake()
+        {
+            panel = GetComponent<RectTransform>();
+            ApplySafeArea();
+        }
+
+        private void ApplySafeArea()
+        {
+            Rect safeArea = Screen.safeArea;
+
+            Vector2 anchorMin = safeArea.position;
+            Vector2 anchorMax = safeArea.position + safeArea.size;
+
+            anchorMin.x /= Screen.width;
+            anchorMin.y /= Screen.height;
+            anchorMax.x /= Screen.width;
+            anchorMax.y /= Screen.height;
+
+            panel.anchorMin = anchorMin;
+            panel.anchorMax = anchorMax;
+        }
+    }
+}
